0a91290f20c2388f734c9062c3fd9d51825cf06a
aaksul <aksulperen@gmail.com>
2021-10-11 14:12:45 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <pcap.h>
+
+
+int main(int argc, char **argv)
+{
+pcap_t *fp;
+char errbuf[PCAP_ERRBUF_SIZE];
+char source[PCAP_BUF_SIZE];
+char* Fn;
+    if(argc != 2){
+
+        printf("usage: %s filename", argv[0]);
+        return -1;
+
+    }
+
+Fn=argv[1];
+auto x = pcap_open_offline(Fn,errbuf);
+
+if (x==NULL)
+{
+    printf("Couldnt open file %s\n",Fn);
+    return -1;
+}
+
+
+}

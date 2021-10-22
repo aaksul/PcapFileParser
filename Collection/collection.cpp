9f1e4937f997566f3eb06fba1fc0e4f8dab34fb0
aaksul <aksulperen@gmail.com>
2021-10-22 14:05:06 +0300

    Delete Ibuilder and Packet lib

--- Collection/collection.cpp
+++ Collection/collection.cpp
@@ -16,16 +16,17 @@ void Packet_collection::Add_Packet(const struct ip_header* my_ip){
         bool find=false;
         for (auto ptr{begin}; ptr != end && find !=true ; ptr++)
         {
-            auto source=ptr->first;//source ip address
+            auto source=ptr->first;//iterate over map select ip address which has been recorded
+            //Control given as parameter ip_address with selected ip address 
             for (size_t i = 0; i < 4 && source[i] == my_ip->SOURCE_IP_ADRESS[i] ; i++)
             {
-                if(i==3)
+                if(i==3)//be sure all bytes are equal
                 {
                     find=true;
                 }
                 
             }
-            if (find)
+            if (find)//if it is present, not create a new key-value, push to selected key's value
             {
                 ptr->second.push_back(my_ip);
             }
@@ -45,6 +46,7 @@ void Packet_collection::Filter(const char* ip_adress){
     int offset{0};
     int ip_addr[4]={0,0,0,0};
     int ip_off{0};
+    //convert char ip address format to int example: "192.168.1.1" to int[4]{192.168.1.1}
     for (size_t i = 0; i < strlen(ip_adress); i++)
     {
         if(ip_adress[i]==46 || i == strlen(ip_adress)-1)
@@ -68,16 +70,19 @@ void Packet_collection::Filter(const char* ip_adress){
             offset++;
         }
     }
+    
     auto begin{ std::begin(packets) };
     auto end{ std::end(packets) };
-        
+    //filter given ip_address from command line     
     for (auto ptr{begin}; ptr != end ; ptr++)
     {
         if(ptr->first[0]==ip_addr[0] && ptr->first[1]==ip_addr[1] && ptr->first[2]==ip_addr[2] && ptr->first[3]==ip_addr[3])
+        {
             std::cout << ptr->first[0] <<"."<< ptr->first[1] <<"."<< ptr->first[2] <<"."<< ptr->first[3] << "\t has sent " << ptr->second.size() <<" packet" <<"\n" ;         
-        
+            return;
+        }    
     }
-    
+    std::cout<<"No record for given ip\n";
     
 }
 
